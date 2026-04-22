92ad75469345ba141f5f028713931e5ba5fd5f84
ramhuw <johndoubleb@gmail.com>
2026-04-22 01:08:26 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,20 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         input = input.trim().to_string();
-        let mut token_iter = input.split_whitespace().into_iter();
+        let mut token_iter = input.split_whitespace().map(|s| s.to_string());
         let command = token_iter.next().unwrap();
-        match command {
+        match command.as_str() {
             "exit" => break,
             "echo" => {
-                println!("{}", token_iter.map(|s| s.to_string()).collect::<Vec<String>>().join(" "))
+                println!("{}", token_iter.collect::<Vec<String>>().join(" "))
+            },
+            "type" => {
+                let snd_command = token_iter.next().unwrap();
+                match snd_command.as_str() {
+                    "echo" => println!("echo is a shell builtin"),
+                    "exit" => println!("echo is a shell builtin"),
+                    _ => println!("{snd_command}: not found")
+                }
             },
             _ => println!("{}: command not found", command)
         }
