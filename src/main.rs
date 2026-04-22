02e33ab12b2cc70c2c2763237579344f2ecbc9a6
ramhuw <johndoubleb@gmail.com>
2026-04-22 01:03:11 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        command = command.trim().to_string();
-        if command == "exit".to_string() {
-            break;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+        let mut token_iter = input.split_whitespace().into_iter();
+        let command = token_iter.next().unwrap();
+        match command {
+            "exit" => break,
+            "echo" => {
+                println!("{}", token_iter.map(|s| s.to_string()).collect::<Vec<String>>().join(" "))
+            },
+            _ => println!("{}: command not found", command)
         }
-        println!("{command}: command not found");
     }
     
 }
